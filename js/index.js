9bcddc3f29893729734353444a79fa6485ce3778
Aurora Dai <112133462+AuroraDai@users.noreply.github.com>
2022-09-20 11:52:48 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -68,22 +68,5 @@ var timer = setInterval(function () {
 
 }, 4000)
 
-//responsive navbar
-// let hamburger = document.querySelector('.hamburger');
-// let mobile_menu = document.querySelector('.nav_list');
-// let navBar = document.querySelector('.navBar');
-// let nav_ul = document.querySelector('.nav_ul')
 
-
-// hamburger.addEventListener('click', () => {
-//     hamburger.classList.toggle('active');
-//     mobile_menu.classList.toggle('active');
-
-
-// })
-
-// mobile_menu.addEventListener('click', () => {
-//     nav_ul.classList.toggle('active');
-//     hamburger.classList.toggle('active');
-// })
 
